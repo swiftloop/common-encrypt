68e508a034ee24285a0838225093cdb999df4fe2
apple <apple@appledeMacBook-Pro.local>
2019-03-22 16:07:53 +0800

    Initial commit

---
+++ src/main/java/win/oscene/App.java
@@ -0,0 +1,13 @@
+package win.oscene;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
